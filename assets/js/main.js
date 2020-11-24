58adf27bfe3daa68a184ca2021b5a5537b361d01
jessiejones <jessie_jones@iinet.net.au>
2020-11-24 19:06:23 +1100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,194 @@
+
+$(document).ready(function(){
+	console.log("welcome");
+
+$(".info").addClass("hide");
+
+
+
+	$(".btn-warm").click(function(){
+		$(".cold").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".number").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".warm").removeClass("hide");
+	});
+
+	$(".btn-cold").click(function(){
+		$(".warm").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".number").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".cold").removeClass("hide");
+	});
+
+	$(".btn-gate").click(function(){
+		$(".cold").addClass("hide");
+		$(".warm").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".number").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".gate").removeClass("hide");
+	});
+
+	$(".btn-fence").click(function(){
+		$(".cold").addClass("hide");
+		$(".warm").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".number").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".fence").removeClass("hide");
+	});
+
+	$(".btn-knocker").click(function(){
+		$(".cold").addClass("hide");
+		$(".warm").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".number").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".knocker").removeClass("hide");
+	});
+
+	$(".btn-doorbell").click(function(){
+		$(".cold").addClass("hide");
+		$(".warm").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".number").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".doorbell").removeClass("hide");
+	});
+
+	$(".btn-nosales").click(function(){
+		$(".cold").addClass("hide");
+		$(".warm").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".number").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".nosales").removeClass("hide");
+	});
+
+	$(".btn-letter").click(function(){
+		$(".cold").addClass("hide");
+		$(".warm").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".number").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".letter").removeClass("hide");
+	});
+
+	$(".btn-anonymous").click(function(){
+		$(".cold").addClass("hide");
+		$(".warm").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".number").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".anonymous").removeClass("hide");
+	});
+
+	$(".btn-number").click(function(){
+		$(".cold").addClass("hide");
+		$(".warm").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".info").addClass("hide");
+
+		$(".number").removeClass("hide");
+	});
+
+	$(".btn-reset").click(function(){
+		$(".cold").removeClass("hide");
+		$(".warm").removeClass("hide");
+		$(".gate").removeClass("hide");
+		$(".fence").removeClass("hide");
+		$(".knocker").removeClass("hide");
+		$(".doorbell").removeClass("hide");
+		$(".nosales").removeClass("hide");
+		$(".letter").removeClass("hide");
+		$(".anonymous").removeClass("hide");
+		$(".number").removeClass("hide");
+		$(".info").addClass("hide");
+	});
+
+	$(".btn-info").click(function(){
+		$(".cold").addClass("hide");
+		$(".warm").addClass("hide");
+		$(".gate").addClass("hide");
+		$(".fence").addClass("hide");
+		$(".knocker").addClass("hide");
+		$(".doorbell").addClass("hide");
+		$(".nosales").addClass("hide");
+		$(".letter").addClass("hide");
+		$(".anonymous").addClass("hide");
+		$(".number").addClass("hide");
+
+		$(".info").removeClass("hide");
+	});
+
+});
+
+
+
+
+
+
